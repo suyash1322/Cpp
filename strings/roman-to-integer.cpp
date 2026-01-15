a6534a56c5f2784e3257fe4a674d234d0c5c29c2
Suyash Rao <suyashrao1310@gmail.com>
2026-01-15 21:01:20 +0530

    Add files via upload

---
+++ strings/roman-to-integer.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int romanToInteger(string s)
+{
+    unordered_map<char, int> map;
+    map['I'] = 1;
+    map['V'] = 5;
+    map['X'] = 10;
+    map['L'] = 50;
+    map['C'] = 100;
+    map['D'] = 500;
+    map['M'] = 1000;
+
+    int ans = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (map[s[i]] < map[s[i + 1]])
+        {
+            ans -= map[s[i]];
+        }
+        else
+        {
+            ans += map[s[i]];
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    cout << romanToInteger(s) << endl;
+
+    return 0;
+}
